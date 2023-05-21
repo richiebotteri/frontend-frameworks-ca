f6e2df7786791a870d02d9329cd2ba4f3b3aae7a
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-05-21 14:18:32 +0200

    refactor main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/ContactPage" element={<ContactPage />} />
+        <Route index element={<HomePage />} />
       </Routes>
     </Layout>
   );
