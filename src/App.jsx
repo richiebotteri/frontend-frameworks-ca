42e4759a9911cbc1a46332fb14b20c6133cc0ab7
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-05-23 12:15:51 +0200

    updating render files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout";
 import ContactPage from "./pages/ContactPage";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/ContactPage" element={<ContactPage />} />
-        <Route index element={<HomePage />} />
-      </Routes>
-    </Layout>
+    <Routes>
+      <Route path="/ContactPage" element={<ContactPage />} />
+      <Route index element={<HomePage />} />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App.jsx";
 import GlobalStyle from "./styles/GlobalStyle.jsx";
 import Theme from "./styles/theme.jsx";
 import { BrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Theme>
       <GlobalStyle />
       <BrowserRouter>
-        <App />
+        <Layout>
+          <App />
+        </Layout>
       </BrowserRouter>
     </Theme>
   </React.StrictMode>
